a1794573e7246ab157455728945558890be08014
Nat2120 <reyes.sanchez.natalia21@gmail.com>
2026-09-01 11:23:58 -0600

    eliminacion de carpeta y creación de una nueva para la clase

--- 27Agosto/TAREA/edad.js
+++
@@ -1,6 +0,0 @@
-edad = prompt("Ingresa tu edad");
-if(edad >= 18){
-    console.log("Eres mayor de edad , diviertete ");
-}else{
-    console.log("Regresate a tu casa niño");
-}
--- 27Agosto/TAREA/tablas.js
+++
@@ -1,6 +0,0 @@
-var numero = parseInt (prompt("Escribe un numero para hacer tu tabla de multiplicar"));
-var control = 1;
-while(control <= 10){
-    console.log(numero + " * "+ control + " = " + (control*numero));
-    control++;
-}
