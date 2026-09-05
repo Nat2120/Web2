2afdfe4095a139e6bfdaa5b24ca6fad57c93a919
Nat2120 <reyes.sanchez.natalia21@gmail.com>
2026-09-05 13:58:06 -0600

    se corrigio el codigo de juego para que cumpliera con el objetivo

--- 01Septiembre/juego.js
+++ 01Septiembre/juego.js
@@ -1,15 +1,22 @@
-let numeroMaquina = Math.floor(Math.random()*(10 - 1)) + 1;
+let numeroMaquina = Math.floor(Math.random() * (10 - 1)) + 1;
+
 var vidas = 5; 
 
 while(vidas >= 1){
     var numusuario = parseInt(prompt("Adivina el numero del 1 - 10"));
+
     if(numusuario == numeroMaquina){
         console.log("GANASTE");
         break;
     }
     else {
         console.log("Nop, prueba otra vez ");
-        vidas --;
-        console.log("Te quedan " + vidas + "vidas");
+        vidas--;
+        console.log("Te quedan " + vidas + " vidas");
     }
+}
+
+if(vidas == 0){
+    console.log("PERDISTE");
+    console.log("El número era " + numeroMaquina);
 }
